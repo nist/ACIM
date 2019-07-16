b8425f0f40f626254fa89f46737987f057ce2e86
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2019-07-16 16:30:19 -0400

    #10: Show current lesson when user click on ACIM icon.

--- src/main.js
+++ src/main.js
@@ -6,33 +6,40 @@ const MIN_LESSON_ID = 1
 const MAX_LESSON_ID = 365
 
 var lesson = {
-  id: 1,
+  id: MIN_LESSON_ID,
   title: 'Test',
   url: '',
   day: '',
   refresh: function() {
     this.url = chrome.i18n.getMessage('show_url', []) + '-'+ this.id +'.html'
     chrome.browserAction.setBadgeText({text:String(this.id)})
+    chrome.storage.local.set({'lessonId': lesson.id})
   }
 }
 
 // Load current lesson
 if (localStorage.hasOwnProperty('lessonId')) {
   chrome.storage.local.get(['lessonId'], function(result) {
-        lesson.id = result.key
+        lesson.id = result.lessonId || MIN_LESSON_ID
   });
 } else {
-  lesson.id = 1
+  lesson.id = MIN_LESSON_ID
 }
 
 // If we are a following day, go to next lesson.
 if (localStorage.hasOwnProperty('lastDay')) {
-  chrome.storage.local.get(['lastDay'], function(result) {
+  chrome.storage.sync.get(['lastDay'], function(result) {
         lesson.day = result.key
   });
 } else {
   lesson.day = 1
 }
-chrome.storage.sync.set({'lessonId': lesson.id})
 
 lesson.refresh()
+
+chrome.browserAction.onClicked.addListener(function show() {
+  var show_tab = {
+    'url' : lesson.url
+  }
+  chrome.tabs.create(show_tab)
+});
